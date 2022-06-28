a6c390c14edaa1ce20e867f8d55ea0057fb80cda
Rudra Shende <63746650+hypercoder457@users.noreply.github.com>
2022-06-28 13:06:05 +0530

    Turn LoadingScreen into function from class

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,31 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import firebase from "firebase";
 
-export default class LoadingScreen extends React.Component {
-    componentDidMount() {
-        this.checkIfLoggedIn();
-    }
-
-    checkIfLoggedIn = () => {
+export default function LoadingScreen(props) {
+    useEffect(() => {
         firebase
             .auth()
             .onAuthStateChanged(user => {
-            if (user) {
-                this.props.navigation.navigate("DashboardScreen");
-            } else {
-                this.props.navigation.navigate("LoginScreen");
-            }
-        });
-    };
+                if (user) {
+                    props.navigation.navigate("DashboardScreen");
+                } else {
+                    props.navigation.navigate("LoginScreen");
+                }
+            })
+    }, [])
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Loading...</Text>
-            </View>
-        );
-    }
+    return (
+        <View style={styles.container}>
+            <Text>Loading...</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
